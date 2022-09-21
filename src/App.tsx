320f7aef4af3171a6d55a86c11e7e5f7d573e4b3
Itzhak Franco <itzhakfranco@gmail.com>
2022-09-21 16:00:35 +0300

    done

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addReservation } from "./store/slices/reservationSlice";
+import { RootState } from "./store/store";
+import ReservationCard from "./components/ReservationCard";
+import CustomerCard from "./components/CustomerCard";
+import "./App.css";
+function App() {
+	const dispatch = useDispatch();
+
+	const [reservationNameInput, setReservationNameInput] = useState("");
+
+	const reservations = useSelector(
+		(state: RootState) => state.reservations.value
+	);
+	const customers = useSelector((state: RootState) => state.customer.value);
+	const handleAddReservations = () => {
+		dispatch(addReservation(reservationNameInput));
+	};
+
+	return (
+		<div className='App'>
+			<div className='container'>
+				<div className='reservation-container'>
+					<div>
+						<h5 className='reservation-header'>Reservations</h5>
+						<div className='reservation-cards-container'>
+							{reservations.map((name, index) => {
+								return <ReservationCard name={name} index={index} />;
+							})}
+						</div>
+					</div>
+					<div className='reservation-input-container'>
+						<input
+							value={reservationNameInput}
+							onChange={(e) => {
+								setReservationNameInput(e.target.value);
+							}}
+						/>
+						<button onClick={handleAddReservations}>Add</button>
+					</div>
+				</div>
+				<div className='customer-food-container'>
+					{customers.map((customer) => {
+						return (
+							<CustomerCard
+								id={customer.id}
+								name={customer.name}
+								food={customer.food}
+								key={customer.id}
+							/>
+						);
+					})}
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
